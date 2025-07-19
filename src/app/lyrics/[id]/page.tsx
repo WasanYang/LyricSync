c67e72cd0cfe12351e2c2af80e1661bdbf3e6eba
Es Yaj <esxy26@gmail.com>
2025-07-19 01:43:09 +0000

    ทำไมพอจอใหญ่ทำให้มี scroll แนวนอน

--- src/app/lyrics/[id]/page.tsx
+++ src/app/lyrics/[id]/page.tsx
@@ -12,14 +12,14 @@ interface LyricPageProps {
 }
 
 export default async function LyricPage({ params }: LyricPageProps) {
-  const song = getSongById(params.id);
+  const song = await getSongById(params.id);
 
   if (!song) {
     notFound();
   }
 
   return (
-    <div className="relative w-screen h-screen flex items-center justify-center bg-background">
+    <div className="relative w-full h-screen flex items-center justify-center bg-background">
        <Button asChild variant="ghost" className="absolute top-4 left-4 z-10">
         <Link href="/">
           <ArrowLeft className="mr-2 h-4 w-4" /> Back to Home
