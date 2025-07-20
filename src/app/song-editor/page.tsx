4cb32967b456501b706752278c87f4ca4666d283
Es Yaj <esxy26@gmail.com>
2025-07-20 11:55:18 +0000

    ทำไมเข้าหน้าสร้างเพลงไม่ได้

--- src/app/song-editor/page.tsx
+++ src/app/song-editor/page.tsx
@@ -7,15 +7,15 @@ import SongCreator from "@/components/SongCreator";
 function SongCreatorPageContent() {
   return (
     <div className="flex-grow flex flex-col h-screen bg-background">
-      <SongCreator />
+       <Suspense fallback={<div>Loading editor...</div>}>
+          <SongCreator />
+       </Suspense>
     </div>
   );
 }
 
 export default function SongCreatorPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <SongCreatorPageContent />
-    </Suspense>
   )
 }
