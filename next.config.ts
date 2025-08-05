b14ed7c39935e544df2631fbdad287c395a0111e
Es Yaj <esxy26@gmail.com>
2025-08-05 15:59:15 +0000

    pwa web จะทำได้ใช่ไหม

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,55 @@
+
 import type { NextConfig } from 'next';
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  scope: '/',
+  start_url: '/',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  extendManifest: (manifest) => {
+    manifest.id = '/';
+    manifest.shortcuts = [
+      {
+        name: 'Search Songs',
+        short_name: 'Search',
+        description: 'Search for songs and setlists',
+        url: '/search',
+        icons: [{ src: '/icons/search.png', sizes: '192x192' }],
+      },
+      {
+        name: 'My Setlists',
+        short_name: 'Setlists',
+        description: 'View and manage your setlists',
+        url: '/setlists',
+        icons: [{ src: '/icons/setlist.png', sizes: '192x192' }],
+      },
+      {
+        name: 'Create Setlist',
+        short_name: 'Create',
+        description: 'Create a new setlist',
+        url: '/setlists/create',
+        icons: [{ src: '/icons/create.png', sizes: '192x192' }],
+      },
+    ];
+    manifest.share_target = {
+      action: '/shared/target',
+      method: 'GET',
+      params: {
+        title: 'title',
+        text: 'text',
+        url: 'url',
+      },
+    };
+    return manifest;
+  },
 });
 
 import createNextIntlPlugin from 'next-intl/plugin';
